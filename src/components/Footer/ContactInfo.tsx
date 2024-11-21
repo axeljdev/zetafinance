7852d64f1e9d6e70367fe02f486e0496b5162375
Axel <a.janiak@genieclimfrance.fr>
2024-11-21 16:04:40 +0100

    ?

--- src/components/Footer/ContactInfo.tsx
+++ src/components/Footer/ContactInfo.tsx
@@ -1,14 +1,18 @@
-
-
 function ContactInfo() {
   return (
     <div className="flex flex-col items-center lg:items-start">
       <p className="uppercase pb-2 font-semibold">Nous contacter</p>
-      <a href="tel:0972606443" className="hoverRed font-normal">09 72 60 64 43</a>
-      <a href="tel:0769762636" className="hoverRed font-normal">07 69 76 26 36</a>
-      <a href="mailto:contact@zetafinance.fr" className="hoverRed font-normal">contact@zetafinance.fr</a>
+      <a href="tel:0972606443" className="hoverRed font-normal">
+        09 72 60 64 43
+      </a>
+      <a href="tel:0769762636" className="hoverRed font-normal">
+        07 69 76 26 36
+      </a>
+      <a href="mailto:contact@zetafinance.fr" className="hoverRed font-normal">
+        contact@zetafinance.fr
+      </a>
     </div>
   );
 }
 
-export default ContactInfo; 
+export default ContactInfo;

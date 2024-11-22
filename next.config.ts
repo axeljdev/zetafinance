ffe2f7df99d06c2eb132f30e61cecc43d3107774
Axel <a.janiak@genieclimfrance.fr>
2024-11-22 14:23:08 +0100

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,6 @@ const nextConfig = {
     config.resolve.fallback = { fs: false };
     return config;
   },
-  optimizeFonts: true,
-  experimental: {
-    optimizeCss: true,
-  },
 };
 
 module.exports = nextConfig;
